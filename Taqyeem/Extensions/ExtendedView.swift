9546fad7a37acf6b2ebe1a5c0b49db90b8f902e2
Mostafa <mostafa@aqwas.sa>
2020-01-18 22:59:29 +0200

    updates

--- Taqyeem/Extensions/ExtendedView.swift
+++ Taqyeem/Extensions/ExtendedView.swift
@@ -1,27 +1,8 @@
 import Foundation
 import UIKit
 
-class ExtendedView:UIView,GradientProtocol,CircleP
+class ExtendedView:UIView,CircleP
 {
- 
-    var gradient = CAGradientLayer()
- 
-    @IBInspectable var gX1: Double = 0
-    @IBInspectable var gY1: Double = 0
-    @IBInspectable var gX2: Double = 0
-    @IBInspectable var gY2: Double = 0
-    
-    @IBInspectable var horizontalGradient:Bool  = false
-    @IBInspectable var customGradient: Bool = false
-    @IBInspectable var gradientColorOne:UIColor = .clear
-    @IBInspectable var gradientColorTwo:UIColor = .clear
-    @IBInspectable var gradientBorder:Bool = false
-    {
-        didSet
-        {
-           configureGradient(gradientBorder)
-        }
-    }
     @IBInspectable var makeCircle:Bool = false
     {
         didSet
@@ -29,17 +10,6 @@ class ExtendedView:UIView,GradientProtocol,CircleP
             configureCircle()
         }
     }
-    
-    @IBInspectable var gradientBackground:Bool = false
-    {
-        didSet
-        {
-            configureGradient(gradientBackground)
-        }
-    }
-    
-  
-    
 }
 
 
